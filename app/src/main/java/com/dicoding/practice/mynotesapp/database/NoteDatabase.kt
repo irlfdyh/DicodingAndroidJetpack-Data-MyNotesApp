56f58cce6bc739b695fe943f00dc8f045421440e
irshalfir <irshalfir@gmail.com>
2020-09-29 10:11:30 +0700

    create room database, dao, and entity

---
+++ app/src/main/java/com/dicoding/practice/mynotesapp/database/NoteDatabase.kt
@@ -0,0 +1,33 @@
+package com.dicoding.practice.mynotesapp.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun noteDao(): NoteDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: NoteDatabase? = null
+
+        @JvmStatic
+        fun getDatabase(context: Context): NoteDatabase {
+            if (INSTANCE == null) {
+                synchronized(NoteDatabase::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE  = Room.databaseBuilder(
+                            context.applicationContext,
+                            NoteDatabase::class.java,
+                            "note_database"
+                        ).build()
+                    }
+                }
+            }
+            return INSTANCE as NoteDatabase
+        }
+    }
+
+}
